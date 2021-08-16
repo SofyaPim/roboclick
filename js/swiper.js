71d289c60868b160d64ef8e1deb81ffdff0732f6
Sofya <48826470+SofyaPim@users.noreply.github.com>
2021-08-16 07:57:56 +0300

    change transition

--- js/swiper.js
+++ js/swiper.js
@@ -1,10 +1,13 @@
 
 
 var swiper = new Swiper(".mySwiper", {
-    slidesPerView: 3,
+    slidesPerView: "auto",
+        centeredSlides: true,
+        
+   
     spaceBetween: 30,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
+    // pagination: {
+    //   el: ".swiper-pagination",
+    //   clickable: true,
+    // },
   });
