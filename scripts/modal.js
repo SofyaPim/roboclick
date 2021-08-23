e6f4de7e1a08bb937dcb1f9416bcff9284503c71
SofyaPim <sofanpim@mail.ru>
2021-08-23 16:02:53 +0300

    add popup header

--- scripts/modal.js
+++ scripts/modal.js
@@ -1,37 +1,40 @@
 const modals = (triggerSelector, modalSelector, closeSelector, tariffName = '', clickModal = true) => {
     const trigger = document.querySelectorAll(triggerSelector),
         modal = document.querySelector(modalSelector),
+        modalHeader = modal.querySelector('.popup-title'),
         close = document.querySelector(closeSelector),
         modalProto = document.querySelectorAll('[data-modal]');
-
+ let smallHeader = document.createElement('h4');
 
     trigger.forEach(item => {
         item.addEventListener('click', (e) => {
             if (e.target) {
                 e.preventDefault();
             }
+
+            // обращение к родителю
+            let title = item.parentElement.parentElement;
+
+           
+            if (title.querySelector('.card-title')) {
+
+                let tarifname = title.querySelector('.card-title').innerText;
+                smallHeader.innerHTML = `Вы выбрали тариф ${tarifname}`;
+
+
+
+               // smallHeader.innerHTML.insertAdjacentHTML('afterend', '<h3 class="popup-title">Оформление заказа</h3>');
+               modalHeader.append(smallHeader); //добавление названия тарифа
+                console.log(tarifname); // вывод  названия тарифа в консоль
+
+            }
+
             modalProto.forEach(item => {
                 item.style.display = 'none';
+
             })
             modal.style.display = 'block';
             document.body.style.overflow = 'hidden';
-            
-            // try {
-            //     const tarif = document.querySelectorAll(tariffName);
-            //     tarif.forEach(item => {
-            //         let personalTarif = item.innerHTML;
-            //         console.log(personalTarif);
-
-            //         try {
-            //     const cardTitle = document.querySelectorAll('.card-title');
-            //     cardTitle.forEach(item => {
-            //         let title = item.innerHTML;
-            //         console.log(title);
-            //     })
-            // } catch (e) {}
-                    
-            //     })
-            // } catch (e) {}
 
         })
 
@@ -40,19 +43,26 @@ const modals = (triggerSelector, modalSelector, closeSelector, tariffName = '', 
 
         modalProto.forEach(item => {
             item.style.display = 'none';
-        })
 
+
+        })
+        smallHeader.remove();
         modal.style.display = 'none';
         document.body.style.overflow = '';
     })
     modal.addEventListener('click', (e) => {
+
         if (e.target == modal && clickModal) {
 
             modalProto.forEach(item => {
                 item.style.display = 'none';
+
+
             })
+            smallHeader.remove();
             modal.style.display = 'none';
             document.body.style.overflow = '';
+
         }
 
 
