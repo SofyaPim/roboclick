f229bbcd654107521bff3cf97832d90f589aff1b
SofyaPim <sofanpim@mail.ru>
2021-09-06 12:44:05 +0300

    add min-height u-list

--- scripts/swiper.js
+++ scripts/swiper.js
@@ -1,3 +1,37 @@
+// window.addEventListener('resize', resizeHandlerSlider);
+// const slider = document.querySelector('#slider');
+// let { clientWidth } = document.body;
+// let swiper;
+// const sliderInit = () => {
+//  swiper = new Swiper(".pricesSwiper", {
+//     slidesPerView: 1.1,
+//     spaceBetween: 30,
+    
+//     // freeMode: true,
+//     initialSlide: 2,
+//     centeredSlides: true,
+    
+//     pagination: {
+//       el: ".swiper-pagination",
+//       clickable: true,
+//     }
+//   })
+// }
+
+// const resizeHandlerSlider = () => {
+//   if (clientWidth !== document.body.clientWidth) {
+//       clientWidth = document.body.clientWidth;
+
+//       if (swiper) {
+//           swiper.destroy();
+//       }
+
+//       sliderInit();
+//   }
+// }
+
+
+
 const swiper = new Swiper(".pricesSwiper", {
     slidesPerView: 1.1,
     spaceBetween: 30,
