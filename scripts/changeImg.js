24084783e5f63148eafdd2806a77137e557558b1
SofyaPim <sofanpim@mail.ru>
2021-09-14 15:41:38 +0300

    resizeble img stage-content

--- scripts/changeImg.js
+++ scripts/changeImg.js
@@ -3,17 +3,28 @@ function changeImg() {
 
     let contentBlock = document.querySelector('#stage-content');
     let meeting = document.querySelector('#meeting');
-    // let  tariff = document.querySelector('#tariff');
-    // let  payment = document.querySelector('#payment');
-    // let  learning = document.querySelector('#learning');
-    // let  money = document.querySelector('#money');
- window.addEventListener('resize', () => {
+     let  tariff = document.querySelector('#tariff');
+     let  payment = document.querySelector('#payment');
+     let  learning = document.querySelector('#learning');
+     let  money = document.querySelector('#money');
+ 
       if(contentBlock.clientWidth  <= 440){
+
+        
     meeting.style.backgroundImage = "url('../Roboclick/images/stage-meeting-app.jpg')";
+    tariff.style.backgroundImage = "url('../Roboclick/images/stage-tariff-app.jpg')";
+    money.style.backgroundImage = "url('../Roboclick/images/stage-money-app.jpg')";
+    payment.style.backgroundImage = "url('../Roboclick/images/stage-payment-app.jpg')";
+    learning.style.backgroundImage = "url('../Roboclick/images/mashine-learning-app.jpg')";
+
 }else{
     meeting.style.backgroundImage = "url('../Roboclick/images/stage-meeting.jpg')";
+    tariff.style.backgroundImage = "url('../Roboclick/images/stage-tariff.jpg')";
+    money.style.backgroundImage = "url('../Roboclick/images/stage-money.jpg')";
+    payment.style.backgroundImage = "url('../Roboclick/images/stage-payment.jpg')";
+    learning.style.backgroundImage = "url('../Roboclick/images/mashine-learning.jpg')";
 }
- })
+ 
        
    
 
