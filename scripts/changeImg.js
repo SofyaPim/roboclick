3d0e82db1e50eafadc16fc09949f3b2559e6edb6
SofyaPim <sofanpim@mail.ru>
2021-09-14 15:51:17 +0300

    change img path workwithus

--- scripts/changeImg.js
+++ scripts/changeImg.js
@@ -10,19 +10,35 @@ function changeImg() {
  
       if(contentBlock.clientWidth  <= 440){
 
-        
-    meeting.style.backgroundImage = "url('../Roboclick/images/stage-meeting-app.jpg')";
-    tariff.style.backgroundImage = "url('../Roboclick/images/stage-tariff-app.jpg')";
-    money.style.backgroundImage = "url('../Roboclick/images/stage-money-app.jpg')";
-    payment.style.backgroundImage = "url('../Roboclick/images/stage-payment-app.jpg')";
-    learning.style.backgroundImage = "url('../Roboclick/images/mashine-learning-app.jpg')";
+    //to localhost
+    // meeting.style.backgroundImage = "url('../Roboclick/images/stage-meeting-app.jpg')";
+    // tariff.style.backgroundImage = "url('../Roboclick/images/stage-tariff-app.jpg')";
+    // money.style.backgroundImage = "url('../Roboclick/images/stage-money-app.jpg')";
+    // payment.style.backgroundImage = "url('../Roboclick/images/stage-payment-app.jpg')";
+    // learning.style.backgroundImage = "url('../Roboclick/images/mashine-learning-app.jpg')";
+
+    //to server
+    meeting.style.backgroundImage = "url('images/stage-meeting-app.jpg')";
+    tariff.style.backgroundImage = "url('images/stage-tariff-app.jpg')";
+    money.style.backgroundImage = "url('images/stage-money-app.jpg')";
+    payment.style.backgroundImage = "url('images/stage-payment-app.jpg')";
+    learning.style.backgroundImage = "url('images/mashine-learning-app.jpg')";
+
 
 }else{
-    meeting.style.backgroundImage = "url('../Roboclick/images/stage-meeting.jpg')";
-    tariff.style.backgroundImage = "url('../Roboclick/images/stage-tariff.jpg')";
-    money.style.backgroundImage = "url('../Roboclick/images/stage-money.jpg')";
-    payment.style.backgroundImage = "url('../Roboclick/images/stage-payment.jpg')";
-    learning.style.backgroundImage = "url('../Roboclick/images/mashine-learning.jpg')";
+      //to localhost
+    // meeting.style.backgroundImage = "url('../Roboclick/images/stage-meeting.jpg')";
+    // tariff.style.backgroundImage = "url('../Roboclick/images/stage-tariff.jpg')";
+    // money.style.backgroundImage = "url('../Roboclick/images/stage-money.jpg')";
+    // payment.style.backgroundImage = "url('../Roboclick/images/stage-payment.jpg')";
+    // learning.style.backgroundImage = "url('../Roboclick/images/mashine-learning.jpg')";
+
+//to server
+    meeting.style.backgroundImage = "url('images/stage-meeting.jpg')";
+    tariff.style.backgroundImage = "url('images/stage-tariff.jpg')";
+    money.style.backgroundImage = "url('images/stage-money.jpg')";
+    payment.style.backgroundImage = "url('images/stage-payment.jpg')";
+    learning.style.backgroundImage = "url('images/mashine-learning.jpg')";
 }
  
        
