7aebf246b93a489c1d318ab7230ae41deb839790
SofyaPim <sofanpim@mail.ru>
2021-09-14 13:23:03 +0300

    compress jsfiles

--- scripts/phone-mask.js
+++ scripts/phone-mask.js
@@ -2,17 +2,3 @@ function phoneMask(){document.querySelectorAll("input[type='tel']").forEach(e=>{
  
 
 
-// function phoneMask() {
-//    let selector = document.querySelectorAll("input[type='tel']");
-// selector.forEach( item => {
-//     let im = new Inputmask("+7(999)999-99-99",);
-//     im.mask(item);
-// }) 
-    
-// }
-
-// export default phoneMask;
-
-
-
-//{ "clearIncomplete": true }
