95dcf2aeae6d22b170d80abc327f0b364b58deb9
SofyaPim <sofanpim@mail.ru>
2021-09-17 10:17:05 +0300

    runnum once run

--- scripts/showStatistic.js
+++ scripts/showStatistic.js
@@ -1,21 +1,20 @@
 function showStatistic(section) {
 
-    let time = 1000;
-    let step = 10;
-
-    function runNum(num, elem, step = 1) {
-
+    let time = 1500;
+    function runNum(num, elem, speed) {
         let l = document.querySelector(elem);
         let n = 0;
-        let t = Math.round(time / (num / step));
+        let step = num / (time / speed);
+        //  let t = Math.round(time/(num/step));
         let interval = setInterval(() => {
             n = n + step;
-            if (n === num) {
+            if (n >= num) {
+                l.innerHTML = num;
                 clearInterval(interval);
+                return;
             }
-
-            l.innerHTML = n;
-        }, t)
+            l.innerHTML = Math.floor(n);
+        }, speed)
     }
 
 
@@ -23,18 +22,17 @@ function showStatistic(section) {
     document.addEventListener('scroll', () => {
         let pageItem = document.querySelector(section);
         let pageItemTop = pageItem.getBoundingClientRect().top;
-       // let pageItemBottom = pageItem.getBoundingClientRect().bottom;
-
         if (pageItemTop > 50 && pageItemTop < 160) {
-            runNum(1000, '#clients', 10);
-            runNum(80, '#traffic', 2);
+            runNum(1000, '#clients', 70);
+            runNum(80, '#traffic', 12);
+            runNum = function () {}
             console.log('gotcha!');
-        } 
-         console.log(`Верхняя граница элемента - ${pageItemTop}`);
+        }
+        console.log(`Верхняя граница элемента - ${pageItemTop}`);
     })
-    
 
-     
+
+
 
 }
 export default showStatistic;
