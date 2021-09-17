90ed331b4d0f7232e194cd415ef9b952ff9f7b1f
SofyaPim <sofanpim@mail.ru>
2021-09-17 10:25:41 +0300

    added lessnum

--- scripts/showStatistic.js
+++ scripts/showStatistic.js
@@ -5,7 +5,6 @@ function showStatistic(section) {
         let l = document.querySelector(elem);
         let n = 0;
         let step = num / (time / speed);
-        //  let t = Math.round(time/(num/step));
         let interval = setInterval(() => {
             n = n + step;
             if (n >= num) {
@@ -16,6 +15,21 @@ function showStatistic(section) {
             l.innerHTML = Math.floor(n);
         }, speed)
     }
+    function lessNum(num, elem, speed) {
+        let l = document.querySelector(elem);
+        let n = 999;
+        let step = num / (time / speed);
+        let interval = setInterval(() => {
+            n = n - step;
+            if (n <= num) {
+                l.innerHTML = num;
+                clearInterval(interval);
+                return;
+            }
+            l.innerHTML = Math.floor(n);
+        }, speed)
+    }
+
 
 
 
@@ -25,7 +39,10 @@ function showStatistic(section) {
         if (pageItemTop > 50 && pageItemTop < 160) {
             runNum(1000, '#clients', 70);
             runNum(80, '#traffic', 12);
+            runNum(2, '#conversion', 20);
             runNum = function () {}
+            lessNum(500, '#minPrice', 45);
+            lessNum = function () {}
             console.log('gotcha!');
         }
         console.log(`Верхняя граница элемента - ${pageItemTop}`);
