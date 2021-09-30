e706e021d9a4be0f4ab97ae17ebdbb06b7753cc4
SofyaPim <sofanpim@mail.ru>
2021-09-30 18:53:28 +0300

    modifed scss focus supportcard 1024

--- scripts/showDesc.js
+++ scripts/showDesc.js
@@ -3,52 +3,35 @@ function showDesk(selectors, contentblock) {
 
     function desktopEvents(el, content) {
         el.addEventListener('mouseover', () => {
-       
+
             content.style.opacity = 1;
-           })
-           el.addEventListener('mouseout', () => {
-         
+        })
+        el.addEventListener('mouseout', () => {
+
             content.style.opacity = 0;
         })
     }
+
     function mobileEvents(el, content) {
         el.addEventListener('focus', () => {
-       
-            content.style.opacity = 1;
-           })
-           el.addEventListener('blur', () => {
-       
-            content.style.opacity = 0;
-           })
 
-           //may delete
-         
-        // el.addEventListener('touchstart', () => {
-       
-        //     content.style.opacity = 1;
-        //    })
-        //    //may delete
-        //    el.addEventListener('touchend', () => {
-         
-        //     content.style.opacity = 0;
-        // })
+            content.style.opacity = 1;
+        })
+        el.addEventListener('blur', () => {
+
+            content.style.opacity = 0;
+        })
     }
 
     items.forEach(item => {
+        let desc = item.querySelector(contentblock);
+        desc.style.opacity = 0;
+        if (window.innerWidth < 1024) {
+            mobileEvents(item, desc);
 
-       let desc =  item.querySelector(contentblock);
- 
-    
-    desc.style.opacity = 0;
-    if (window.innerWidth < 1000) {
-        mobileEvents(item, desc);
-        
-    }else {
-        desktopEvents(item, desc);
-    }
-     
-  
-
+        } else {
+            desktopEvents(item, desc);
+        }
     })
 }
 export default showDesk;
