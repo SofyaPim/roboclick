2667f8c979d78627f94ccc3eabf62b5ecd3144a4
SofyaPim <sofanpim@mail.ru>
2021-10-01 11:26:31 +0300

    modifed function

--- scripts/showDesc.js
+++ scripts/showDesc.js
@@ -1,36 +1,31 @@
 function showDesk(selectors, contentblock) {
     const items = document.querySelectorAll(selectors);
 
-    function desktopEvents(el, content) {
-        el.addEventListener('mouseover', () => {
 
-            content.style.opacity = 1;
-        })
-        el.addEventListener('mouseout', () => {
-
-            content.style.opacity = 0;
-        })
+    function add(a, b, event) {
+        a.addEventListener(event, () => {
+            b.style.opacity = 1;
+        });
     }
 
-    function mobileEvents(el, content) {
-        el.addEventListener('focus', () => {
-
-            content.style.opacity = 1;
-        })
-        el.addEventListener('blur', () => {
-
-            content.style.opacity = 0;
-        })
+    function remove(a, b, event) {
+        a.addEventListener(event, () => {
+            b.style.opacity = 0;
+        });
     }
+
+  
 
     items.forEach(item => {
         let desc = item.querySelector(contentblock);
         desc.style.opacity = 0;
         if (window.innerWidth < 1024) {
-            mobileEvents(item, desc);
-
+            add(item, desc, 'focus');
+            remove(item, desc, 'blur');
         } else {
-            desktopEvents(item, desc);
+            
+            add(item, desc, 'mouseover');
+            remove(item, desc, 'mouseout');
         }
     })
 }
