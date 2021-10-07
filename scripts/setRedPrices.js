6b762fcf33a0d6f1a41bc2ce68550f205d534040
SofyaPim <sofanpim@mail.ru>
2021-10-07 17:22:37 +0300

    new style at lowPrice

--- scripts/setRedPrices.js
+++ scripts/setRedPrices.js
@@ -10,10 +10,11 @@ function setRedPrices() {
         let discountPrice = price - (price * 5) / 100;
         let lowPrice = document.createElement('h2');
         lowPrice.classList.add('lowPrice');
-        let parent = card.parentNode;
+      //  let parent = card.parentNode;
         lowPrice.innerHTML = `${discountPrice}$`;
-        parent.appendChild(lowPrice);
-        //  console.log(lowPrice);
+     //   parent.appendChild(lowPrice);
+        let newItem = card.insertAdjacentHTML('beforebegin', `<h2 class="lowPrice"> ${discountPrice}$</h2>`);
+          console.log(newItem);
         card.style.opacity = .4;
         card.classList.add('lineTrough');
 
